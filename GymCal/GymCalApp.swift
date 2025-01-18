ad93a1ba056d1f10fe8ccd3efc0e884742534212
Andrew Slifkin <slifkin@gmail.com>
2025-01-18 14:05:44 +0100

    Initial Commit

---
+++ GymCal/GymCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymCalApp.swift
+//  GymCal
+//
+//  Created by Andrew Slifkin on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct GymCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymCalTests/GymCalTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymCalTests.swift
+//  GymCalTests
+//
+//  Created by Andrew Slifkin on 1/18/25.
+//
+
+import Testing
+@testable import GymCal
+
+struct GymCalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
