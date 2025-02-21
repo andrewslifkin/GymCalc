1069ad57130115f384b4eb7156cec20c26b7abd3
Andrew Slifkin <slifkin@gmail.com>
2025-02-21 19:29:01 +0100

    - Fixed the calculation of achievable weights - Added suggested weight for unachievable weights

---
+++ GymCalcPreviewsTests/GymCalcPreviewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymCalcPreviewsTests.swift
+//  GymCalcPreviewsTests
+//
+//  Created by Andrew Slifkin on 2/16/25.
+//
+
+import Testing
+@testable import GymCalcPreviews
+
+struct GymCalcPreviewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
